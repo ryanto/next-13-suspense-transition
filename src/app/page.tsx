d53117328a5e3abf397a44baa45a080104606313
Ryan Toronto <ryanto@gmail.com>
2023-08-11 11:32:31 -0400

    make easier to read

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,30 @@ import { RefreshButton } from "./refresh-button";
 
 export default function Page() {
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 p-6">
+      <p className="leading-normal max-w-[65ch]">
+        There&apos;s a /route1 page in this app that is an 
+        async server component. If you visit /route1 directly
+        then the component does not render its suspense boundaries.
+      </p>
+
+      <p className="leading-normal max-w-[65ch]">
+        However, if you Link to transition to /route1 then the
+        suspenses boundaries are both triggered and displayed.
+      </p>
+
+      <p className="leading-normal max-w-[65ch]">
+        Also, one of the components does not suspend, so I wasn&apos;t
+        expecting its boundary to be displayed.
+      </p>
+
+      <p className="leading-normal max-w-[65ch]">
+        The other component awaits a resolved promise. I also wasn&apos;t
+        expecting its boundary to be displayed.
+      </p>
+
       <div>
-        <Link href="/route1">Go to /route1</Link>
+        <Link prefetch={false} href="/route1" className="text-blue-500">Go to /route1</Link>
       </div>
 
       <div>
--- src/app/refresh-button.tsx
+++ src/app/refresh-button.tsx
@@ -5,7 +5,7 @@ export function RefreshButton() {
   const router = useRouter();
 
   return (
-    <button onClick={() => router.refresh()}>
+    <button onClick={() => router.refresh()} className="border border-gray-200 px-4 py-2 bg-gray-50">
       Call router.refresh()
     </button>
   )
